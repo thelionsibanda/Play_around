9584e146ffa152a09b6f68ea9fd4f9f09483073b
Minenhle <mini.msib@gmail.com>
2018-02-25 14:20:02 +0200

    Varibles file

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            // commentvv
+            #  comment
+            /* comment  */
+            echo "My first php program  <br> <b> What is php </b>";
+        ?>
+    </body>
+</html>
