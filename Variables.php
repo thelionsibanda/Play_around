0d0096e5ed9f374321ee953951e535bf4529b7a3
Minenhle <mini.msib@gmail.com>
2018-02-25 14:24:11 +0200

    Variables file

---
+++ Variables.php
@@ -0,0 +1,19 @@
+<?php
+
+$Name ="Minenhle";
+$Age = 21;
+
+$Age = ($Age*8);
+
+echo "Age :" ,$Age ," Name :",$Name,"<br>";
+echo "Name :$Name <br>";
+echo "Name :" .$Name;
+echo "<br> Go to eve ",'<a href="http://eve.uj.ac.za">eve</a>';
+$google = 'Google Page';
+echo "<br> Go to Google ",'<a href="http://google.com">'.$google.'</a>';
+
+echo '<br>"Quotes"' , '  it\'s a nice day insn\'t ?';
+
+
+?>
+
